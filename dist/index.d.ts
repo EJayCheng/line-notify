b1b2ca14e901e68f20d37be965b848ffecb62a06
EJay <ejay@anyong.com.tw>
2021-07-10 22:01:38 +0800

    v7.0.0

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,3 @@
 export { LineNotify } from "./line-notify";
-export { LineNotifyMessage } from "./line-notify.dto";
+export { LineNotifyMessage, LineNotifyTokenStatus, LineNotifyTokenTargetType, } from "./line-notify.dto";
 export { LineNotifyService, LINE_NOTIFY_TOKEN } from "./line-notify.service";
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export { LineNotify } from "./line-notify";
-export { LineNotifyMessage } from "./line-notify.dto";
+export {
+  LineNotifyMessage,
+  LineNotifyTokenStatus,
+  LineNotifyTokenTargetType,
+} from "./line-notify.dto";
 export { LineNotifyService, LINE_NOTIFY_TOKEN } from "./line-notify.service";
